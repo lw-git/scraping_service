ce7f59fb9eac057b38f74c04a2703e917ed57f16
lw <lw_git@mail.ru>
2020-08-18 12:31:09 +0500

    Added pagination

--- src/scraping/views.py
+++ src/scraping/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from .models import Vacancy
 from .forms import FindForm
@@ -5,9 +6,14 @@ from .forms import FindForm
 
 def home_view(request):
     form = FindForm()
+    return render(request, 'scraping/home.html', {'form': form})
+
+
+def list_view(request):
+    form = FindForm()
     city = request.GET.get('city')
     language = request.GET.get('language')
-    qs = []
+    context = {'city': city, 'language': language, 'form': form}
     if city or language:
         _filter = {}
         if city:
@@ -16,4 +22,10 @@ def home_view(request):
             _filter['language__slug'] = language
 
         qs = Vacancy.objects.filter(**_filter)
-    return render(request, 'scraping/home.html', {'object_list': qs, 'form': form})
+    paginator = Paginator(qs, 10)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context['object_list'] = page_obj
+
+    return render(request, 'scraping/list.html', context)
