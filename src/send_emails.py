de68c9af258fc10d97e7d19353f698d83691f5a0
lw <lw_git@mail.ru>
2020-08-23 12:08:23 +0500

    Added sending an email with errors to the admin

--- src/send_emails.py
+++ src/send_emails.py
@@ -1,5 +1,6 @@
 import os, sys
 import django
+import datetime
 from django.core.mail import EmailMultiAlternatives
 
 from django.contrib.auth import get_user_model
@@ -10,10 +11,16 @@ os.environ["DJANGO_SETTINGS_MODULE"] = "scraping_service.settings"
 
 django.setup()
 
-from scraping.models import Vacancy
-from scraping_service.settings import EMAIL_HOST_USER
-subject = "Рассылка вакансий"
-text_content = "Рассылка вакансий"
+from scraping.models import Vacancy, Error, Url
+from scraping_service.settings import (
+    EMAIL_HOST_USER,
+    EMAIL_HOST, EMAIL_HOST_PASSWORD
+)
+ADMIN_USER = EMAIL_HOST_USER
+
+today = datetime.date.today()
+subject = f"Рассылка вакансий за {today}"
+text_content = f"Рассылка вакансий {today}"
 from_email = EMAIL_HOST_USER
 empty = '<h2>К сожалению на сегодня по Вашим предпочтениям данных нет. </h2>'
 User = get_user_model()
@@ -27,7 +34,7 @@ if users_dct:
     for pair in users_dct.keys():
         params['city_id__in'].append(pair[0])
         params['language_id__in'].append(pair[1])
-    qs = Vacancy.objects.filter(**params).values()[:10]
+    qs = Vacancy.objects.filter(**params, timestamp=today).values()
     vacancies = {}
     for i in qs:
         vacancies.setdefault((i['city_id'], i['language_id']), [])
@@ -46,3 +53,31 @@ if users_dct:
                                          [to])
             msg.attach_alternative(_html, "text/html")
             msg.send()
+
+qs = Error.objects.filter(timestamp=today)
+subject = ''
+text_content = ''
+to = ADMIN_USER
+_html = ''
+if qs.exists():
+    error = qs.first()
+    data = error.data
+    for i in data:
+        _html += f'<p"><a href="{ i["url"] }">Error: { i["title"] }</a></p><br>'
+    subject += f"Ошибки скрапинга {today}"
+    text_content += "Ошибки скрапинга"
+
+qs = Url.objects.all().values('city', 'language')
+urls_dct = {(i['city'], i['language']): True for i in qs}
+urls_err = ''
+for keys in users_dct.keys():
+    if keys not in urls_dct:
+        urls_err += f'<p"> Для города: {keys[0]} и ЯП: {keys[1]} отсутствуют урлы</p><br>'
+if urls_err:
+    subject += ' Отсутствующие урлы'
+    _html += urls_err
+
+if subject:
+    msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
+    msg.attach_alternative(_html, "text/html")
+    msg.send()
