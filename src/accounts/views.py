0458b8e85c0cc8901784ccc78211258b3c8b2030
lw <lw_git@mail.ru>
2020-08-24 10:42:38 +0500

    Added a form for user requests

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout, get_user_model
 from django.contrib import messages
 
-from accounts.forms import UserLoginForm, UserRegistrationForm, UserUpdateForm
+from accounts.forms import UserLoginForm, UserRegistrationForm, UserUpdateForm, \
+    ContactForm
+from scraping.models import Error
 User = get_user_model()
 
 
@@ -36,6 +38,7 @@ def register_view(request):
 
 
 def update_view(request):
+    contact_form = ContactForm()
     if request.user.is_authenticated:
         user = request.user
         if request.method == 'POST':
@@ -52,7 +55,8 @@ def update_view(request):
         form = UserUpdateForm(
             initial={'city': user.city, 'language': user.language,
                      'send_email': user.send_email})
-        return render(request, 'accounts/update.html', {'form': form})
+        return render(request, 'accounts/update.html',
+                      {'form': form, 'contact_form': contact_form})
     else:
         return redirect('accounts:login')
 
@@ -65,3 +69,29 @@ def delete_view(request):
             qs.delete()
             messages.error(request, 'Пользователь удален :(')
     return redirect('home')
+
+
+def contact(request):
+    if request.method == 'POST':
+        contact_form = ContactForm(request.POST or None)
+        if contact_form.is_valid():
+            data = contact_form.cleaned_data
+            city = data.get('city')
+            language = data.get('language')
+            email = data.get('email')
+            qs = Error.objects.filter(timestamp=dt.date.today())
+            if qs.exists():
+                err = qs.first()
+                data = err.data.get('user_data', [])
+                data.append({'city': city, 'email': email, 'language': language})
+                err.data['user_data'] = data
+                err.save()
+            else:
+                data = [{'city': city, 'email': email, 'language': language}]
+                Error(data=f"user_data:{data}").save()
+            messages.success(request, 'Данные отправлены администрации.')
+            return redirect('accounts:update')
+        else:
+            return redirect('accounts:update')
+    else:
+        return redirect('accounts:login')
--- src/run_scraping.py
+++ src/run_scraping.py
@@ -1,5 +1,6 @@
 import asyncio
 import os, sys
+import datetime as dt
 
 from django.contrib.auth import get_user_model
 from django.db import DatabaseError
@@ -38,11 +39,12 @@ def get_urls(_settings):
     url_dct = {(q['city_id'], q['language_id']): q['url_data'] for q in qs}
     urls = []
     for pair in _settings:
-        tmp = {}
-        tmp['city'] = pair[0]
-        tmp['language'] = pair[1]
-        tmp['url_data'] = url_dct[pair]
-        urls.append(tmp)
+        if pair in url_dct:
+            tmp = {}
+            tmp['city'] = pair[0]
+            tmp['language'] = pair[1]
+            tmp['url_data'] = url_dct[pair]
+            urls.append(tmp)
     return urls
 
 
@@ -72,4 +74,10 @@ for job in jobs:
         pass
 
 if errors:
-    er = Error(data=errors).save()
+    qs = Error.objects.filter(timestamp=dt.date.today())
+    if qs.exists():
+        err = qs.first()
+        err.data.update({'errors': errors})
+        err.save()
+    else:
+        er = Error(data=f'errors:{errors}').save()
