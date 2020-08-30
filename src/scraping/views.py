96fd16b7616ba43579ec1ec9f0540002fdb3e0f2
lw <lw_git@mail.ru>
2020-08-30 11:55:36 +0500

    Added list view

--- src/scraping/views.py
+++ src/scraping/views.py
@@ -1,6 +1,6 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
 from .models import Vacancy
 from .forms import FindForm
 
@@ -42,3 +42,35 @@ class VDetail(DetailView):
     queryset = Vacancy.objects.all()
     template_name = 'scraping/detail.html'
     # context_object_name = 'object'
+
+
+class VList(ListView):
+    model = Vacancy
+    template_name = 'scraping/list.html'
+    form = FindForm()
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        context['city'] = self.request.GET.get('city')
+        context['language'] = self.request.GET.get('language')
+        context['form'] = self.form
+        vacancies = self.get_queryset()
+        paginator = Paginator(vacancies, 10)
+        page_number = self.request.GET.get('page', 1)
+        context['object_list'] = paginator.get_page(page_number)
+
+        return context
+
+    def get_queryset(self):
+        city = self.request.GET.get('city')
+        language = self.request.GET.get('language')
+        qs = []
+        if city or language:
+            _filter = {}
+            if city:
+                _filter['city__slug'] = city
+            if language:
+                _filter['language__slug'] = language
+            qs = Vacancy.objects.filter(**_filter)
+        return qs
